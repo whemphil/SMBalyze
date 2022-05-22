5eb7f8d084010412d63cbefa65e69c3839154b06
Wayne Hemphill <wayne.hemphill@colorado.edu>
2022-05-21 21:37:57 -0600

    Add files via upload

--- R/SMB_Analysis.R
+++ R/SMB_Analysis.R
@@ -1,14 +1,30 @@
-id.spots <- function(path.to.file,file.name,time.step,spot.box=6,spot.radius=6,spot.min=300,spot.max=Inf){
-  find.spots <- function(data,box.size,low.lim,high.lim,fill.radius){
-    peeks=splus2R::peaks(data,span = 2*box.size+1,strict = T)*t(splus2R::peaks(t(data),span = 2*box.size+1,strict = T))
-    col.id=matrix(1:ncol(peeks),nrow = nrow(peeks),ncol = ncol(peeks),byrow = TRUE)
-    row.id=matrix(1:nrow(peeks),nrow = nrow(peeks),ncol = ncol(peeks),byrow = FALSE)
-    rows=c(row.id[peeks==1]); cols=c(col.id[peeks==1])
-    vols=rep(0,times=sum(peeks)); for (i in 1:length(vols)){if(rows[i]>fill.radius & rows[i]<(nrow(data)-fill.radius) & cols[i]>fill.radius & cols[i]<(nrow(data)-fill.radius)){vols[i]=sum(data[(rows[i]-fill.radius):(rows[i]+fill.radius),(cols[i]-fill.radius):(cols[i]+fill.radius)])-median(data[(rows[i]-fill.radius):(rows[i]+fill.radius),(cols[i]-fill.radius):(cols[i]+fill.radius)])*(2*fill.radius+1)^2}}
-    spots=list('x'=cols[(vols>=low.lim & vols<=high.lim)],'y'=((nrow(data)+1)-rows)[(vols>=low.lim & vols<=high.lim)],'row'=rows[(vols>=low.lim & vols<=high.lim)],'col'=cols[(vols>=low.lim & vols<=high.lim)],'vol'=vols[(vols>=low.lim & vols<=high.lim)])
-    final=as.data.frame(cbind(spots[['x']],spots[['y']],spots[['row']],spots[['col']],spots[['vol']]));colnames(final)=col.names = c('x','y','row','col','vol')
-    final=final[order(final$vol),]
-    return(final)
+id.spots <- function(path.to.file,file.name,time.step,spot.box=6,spot.radius=6,spot.min=300,spot.max=Inf,spot.picking='composite',r.pick=2.9,min.pick=5){
+  find.spots <- function(data,box.size,low.lim,high.lim,fill.radius,spot.picking,r.pick,min.pick){
+    if (spot.picking=='composite'){
+      peeks=splus2R::peaks(data,span = 2*box.size+1,strict = T)*t(splus2R::peaks(t(data),span = 2*box.size+1,strict = T))
+      col.id=matrix(1:ncol(peeks),nrow = nrow(peeks),ncol = ncol(peeks),byrow = TRUE)
+      row.id=matrix(1:nrow(peeks),nrow = nrow(peeks),ncol = ncol(peeks),byrow = FALSE)
+      rows=c(row.id[peeks==1]); cols=c(col.id[peeks==1])
+      vols=rep(0,times=sum(peeks)); for (i in 1:length(vols)){if(rows[i]>fill.radius & rows[i]<(nrow(data)-fill.radius) & cols[i]>fill.radius & cols[i]<(nrow(data)-fill.radius)){vols[i]=sum(data[(rows[i]-fill.radius):(rows[i]+fill.radius),(cols[i]-fill.radius):(cols[i]+fill.radius)])-median(data[(rows[i]-fill.radius):(rows[i]+fill.radius),(cols[i]-fill.radius):(cols[i]+fill.radius)])*(2*fill.radius+1)^2}}
+      spots=list('x'=cols[(vols>=low.lim & vols<=high.lim)],'y'=((nrow(data)+1)-rows)[(vols>=low.lim & vols<=high.lim)],'row'=rows[(vols>=low.lim & vols<=high.lim)],'col'=cols[(vols>=low.lim & vols<=high.lim)],'vol'=vols[(vols>=low.lim & vols<=high.lim)])
+      final=as.data.frame(cbind(spots[['x']],spots[['y']],spots[['row']],spots[['col']],spots[['vol']]));colnames(final)=col.names = c('x','y','row','col','vol')
+      final=final[order(final$vol),]
+      return(final)
+    }
+    if (spot.picking=='all.frames'){
+      spots=list(NULL)
+      peeks=splus2R::peaks(data,span = 2*box.size+1,strict = T)*aperm(splus2R::peaks(aperm(data,c(2,1,3)),span = 2*box.size+1,strict = T),c(2,1,3))
+      col.id=aperm(array(1:dim(peeks)[2],dim = dim(peeks)[c(2,1,3)]),c(2,1,3))
+      row.id=array(1:dim(peeks)[1],dim = dim(peeks))
+      layer.id=array(rep(1:dim(peeks)[3],each=dim(peeks)[1]*dim(peeks)[2]),dim = dim(peeks))
+      rows=c(row.id[peeks==1]); cols=c(col.id[peeks==1]); layers=c(layer.id[peeks==1])
+      vols=rep(0,times=sum(peeks)); for (j in 1:length(vols)){if(rows[j]>fill.radius & rows[j]<(dim(data)[1]-fill.radius) & cols[j]>fill.radius & cols[j]<(dim(data)[1]-fill.radius)){vols[j]=sum(data[(rows[j]-fill.radius):(rows[j]+fill.radius),(cols[j]-fill.radius):(cols[j]+fill.radius),layers[j]])-median(data[(rows[j]-fill.radius):(rows[j]+fill.radius),(cols[j]-fill.radius):(cols[j]+fill.radius),layers[j]])*(2*fill.radius+1)^2}}
+      spots=data.frame('frame'=layers[(vols>=low.lim & vols<=high.lim)],'x'=cols[(vols>=low.lim & vols<=high.lim)],'y'=((dim(data)[1]+1)-rows)[(vols>=low.lim & vols<=high.lim)],'row'=rows[(vols>=low.lim & vols<=high.lim)],'col'=cols[(vols>=low.lim & vols<=high.lim)],'vol'=vols[(vols>=low.lim & vols<=high.lim)])
+      clusters=dbscan::dbscan(spots[,2:3],eps = r.pick,minPts = min.pick)[['cluster']]
+      particles=aggregate(x=as.data.frame(cbind(spots[,2:6],'cluster'=clusters)),by = cluster,FUN = mean)
+      particles=particles[particles$cluster!=0,]; particles=particles[order(particles$vol),]; particles$col=round(particles$col); particles$row=round(particles$row)
+      return(particles)
+    }
   }
   image.raw=suppressWarnings(tiff::readTIFF(paste0(path.to.file,file.name),all = TRUE,as.is=TRUE))
   pixel.size=dim(image.raw[[1]])
@@ -19,29 +35,49 @@ id.spots <- function(path.to.file,file.name,time.step,spot.box=6,spot.radius=6,s
   suppressWarnings(image(t(pracma::flipud(image.avg)),col=gray.colors(cumprod(pixel.size)),x = 1:pixel.size[2],y=1:pixel.size[1],axes=FALSE,xlab='',ylab=''))
   check.1=readline(prompt = 'Image loading complete. Proceed with spot detection? (y/n):  '); if (check.1=='n'){stop('SCRIPT ABORTED BY USER')}
   #
-  spots=find.spots(image.avg,spot.box,spot.min,spot.max,spot.radius)
+  if (spot.picking=='composite'){
+    spots=find.spots(image.avg,spot.box,spot.min,spot.max,spot.radius,spot.picking,r.pick,min.pick)
+  }
+  if (spot.picking=='all.frames'){
+    spots=find.spots(image.data,spot.box,spot.min,spot.max,spot.radius,spot.picking,r.pick,min.pick)
+  }
   points(spots$x,spots$y,col='red',cex=1.2)
   show(paste0('Median particle intensity = ',median(spots$vol)))
   show(paste0('Number of particles identified = ',length(spots$x)))
   #
   check.2=readline(prompt = 'Is initial particle selection acceptable? (y/n):  '); while (check.2=='n'){
-    spot.box=as.numeric(readline(prompt = 'New spot box size value (ENTER for default):  '))
+    spot.box=as.numeric(readline(prompt = 'New spot selection box size (ENTER for default):  '))
     if (is.na(spot.box)==T){
       spot.box=6
     }
-    spot.radius=as.numeric(readline(prompt = 'New spot radius value (ENTER for default):  '))
+    spot.radius=as.numeric(readline(prompt = 'New spot integration radius (ENTER for default):  '))
     if (is.na(spot.radius)==T){
       spot.radius=6
     }
-    spot.min=as.numeric(readline(prompt = 'New spot minimum threshold (ENTER for default):  '))
+    spot.min=as.numeric(readline(prompt = 'New spot minimum signal (ENTER for default):  '))
     if (is.na(spot.min)==T){
-      spot.min=500
+      spot.min=300
     }
-    spot.max=as.numeric(readline(prompt = 'New spot maximum threshold (ENTER for default):  '))
+    spot.max=as.numeric(readline(prompt = 'New spot maximum signal (ENTER for default):  '))
     if (is.na(spot.max)==T){
       spot.max=Inf
     }
-    spots=find.spots(image.avg,spot.box,spot.min,spot.max,spot.radius)
+    if (spot.picking=='all.frames'){
+      r.pick=as.numeric(readline(prompt = 'New spot merging radius (ENTER for default):  '))
+      if (is.na(r.pick)==T){
+        r.pick=2.9
+      }
+      min.pick=as.numeric(readline(prompt = 'New spot number minimum (ENTER for default):  '))
+      if (is.na(min.pick)==T){
+        min.pick=5
+      }
+    }
+    if (spot.picking=='composite'){
+      spots=find.spots(image.avg,spot.box,spot.min,spot.max,spot.radius,spot.picking,r.pick,min.pick)
+    }
+    if (spot.picking=='all.frames'){
+      spots=find.spots(image.data,spot.box,spot.min,spot.max,spot.radius,spot.picking,r.pick,min.pick)
+    }
     image(t(pracma::flipud(image.avg)),col=gray.colors(cumprod(pixel.size)),x = 1:pixel.size[2],y=1:pixel.size[1],axes=FALSE,xlab='',ylab='')
     points(spots$x,spots$y,col='red',cex=1.2)
     show(paste0('Median particle intensity = ',median(spots$vol)))
